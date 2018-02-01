83e5734140a1bc8e23518ed007da590ab0c46590
php 19 <php.19@catatau.cec.ime.usp.br>
2018-01-31 22:15:00 -0200

    adicionando layout

--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -1,26 +1,17 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Cursos</title>
-    <link rel="stylesheet" type="text/css" href="/css/app.css" />
-  </head>
-  <body>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6 col-md-offset-3">
+@extends('layouts.master')
 
-          <h1>Cursos</h1>
+  @section('content')
+   <div class="row">
+     <div class="col-md-6">
 
-          <ul class="list-group">
-            @foreach ($cursos as $curso)
-              <li class="list-group-item"><a href="/cursos/{{ $loop->index }}">{{ $curso }}</a></li>
-            @endforeach
-          </ul>
+       <h3>Cursos</h3>
 
-        </div>
-      </div>
-    </div>
-    <script type="text/javascript" src="/js/app.js" >
-  </body>
-</html>
+       <ul class="list-group">
+         @foreach ($cursos as $curso)
+           <li class="list-group-item"><a href="/cursos/{{ $loop->index }}">{{ $curso }}</a></li>
+         @endforeach
+       </ul>
+
+     </div>
+   </div>
+  @endsection
